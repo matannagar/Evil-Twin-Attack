196be63c4c813dff532e2ee22b68a612dd4e5dae
matannagar <73398186+matannagar@users.noreply.github.com>
2022-05-01 13:31:14 +0300

    scanning & main documantation

--- main.py
+++ main.py
@@ -1,41 +1,35 @@
 #!/usr/bin/env python
 import os
 import sys
-# import codecs
-# import time
-from colorama import Fore, Style, Back
 from scapy.all import get_if_list
-# from scapy.layers.dot11 import Dot11, Dot11Beacon, Dot11Elt, Dot11ProbeReq , Dot11ProbeResp, Dot11Deauth, RadioTap
 import logging
 
 import fileinput
 import signal
-import colorama
 from sys import platform
 import threading
 import argparse
 from multiprocessing import Process
 
 # import functions
-from configs.config import setMonitorMode,updateHostAP,setManagerMode
-from scanning.sniffing import sniffClients,scanNetworks
+from configs.config import setMonitorMode, updateHostAP, setManagerMode
+from scanning.sniffing import sniffClients, scanNetworks
 from spoofing.spoofing import setTarget
 
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 
 
-
 def attack():
-    
+
     print(f"list of available interfaces:\n{get_if_list()}")
-    
+
     interface = wlan = input("\nEnter full interface name to set to Monitor Mode: \n "
                              "(The full address can be found via the terminal... run command 'pwconfig' and find the desired device) \n \n ")
 
     setMonitorMode(wlan)
 
-    print("\nIn order to change back card into regular wifi-mode run the following command after you are done: \n"
-          "service NetworkManager restart\n")
+    # print("\nIn order to change back card into regular wifi-mode run the following command after you are done: \n"
+    #       "service NetworkManager restart\n")
 
     try:
         known = scanNetworks(interface)
@@ -51,7 +45,7 @@ def attack():
     print('Changing ' + wlan + ' to channel ' + str(known[BSSID][1]))
     os.system("iwconfig %s channel %d" % (wlan, known[BSSID][1]))
 
-    updateHostAP(known,BSSID)
+    updateHostAP(known, BSSID)
 
     sniffClients(wlan, BSSID)
 
@@ -61,21 +55,10 @@ def attack():
     print('Sending deauth packets now, press ctrl+c to end the attack')
     print('' * 2)
 
-    setTarget(brdMac,interface,BSSID)
+    setTarget(brdMac, interface, BSSID)
 
-    while True:
-        ans = input("DONE! do you want to keep attacking? (n/y): ")
-        if(ans == 'y'):
-            setTarget(brdMac,interface,BSSID)
-        else:
-            print("finishing attack")
-            ans2 = input(
-                "do you wish to turn your interface to managed mode again? (y/n):  ")
-            if(ans2 == 'y'):
-                setManagerMode(wlan)
-                break
-            else:
-                break
+    print("DONE! Reverting monitor card back to managed mode :)")
+    setManagerMode(wlan)
 
 
 if __name__ == "__main__":
--- scanning/sniffing.py
+++ scanning/sniffing.py
@@ -1,56 +1,122 @@
 from scapy.all import sniff
-from scapy.layers.dot11 import Dot11,Dot11Beacon, Dot11Elt, Dot11ProbeReq , Dot11ProbeResp
+from scapy.layers.dot11 import Dot11, Dot11Beacon, Dot11Elt, Dot11ProbeReq, Dot11ProbeResp
 import time
 import codecs
 
+'''
+Wireless LAN is a network where devices are using wireless to communicate with each other in a defined area.
+Wireless LAN is ultimately connected to a wired network. 
+
+WAP - wireless access point - a device that accepts wireless signals from multiple devices and retransmits them to the rest of the network.
+AP - access point is layer 2 device because it is like a bridge connecting 2 types of networks, wireless and wired.
+These two networks are not seperate, they belong to one broadcast domain.
+In other word, they belong to one local area network.
+
+802.11 - Wi-Fi - 
+* designed for use in a limited geographical area
+* 
+
+Frames - 
+
+MAC addresses - 
+
+AP broadcast - by default, an AP brodcats its SSID in its service area
+
+BSS - basic service set - is a group of wireless network devices that are working with the same AP
+
+BSSID is a unique identifier used by a client to establish a connection to a particular wireless network.
+It is the phsyical or MAC address which is 48-bit long hexadecimal numbers.
+As a wireless user we cannot see BSSID, but they are included in the packages. 
+
+SSID is the name of a WiFi network that acts as a single shared password between access points and clients.
+
+An AP can provide multiple SSIDs on the same channel through the use of the same or multiple interfaces.
+
+Dot11 is a fast, secure and reliable Wi-Fi service which delivers seamless, building-wide network connectivity in an increasing number of London building centres.
+dot11 = 802.11 specification
+
+Dot11Beacon Frame - Beacon frame is one of the management frames in IEEE 802.11 based WLANs. It contains all the information about the network. 
+Beacon frames are transmitted periodically, they serve to announce the presence of a wireless LAN and to synchronise the members of the service set.
+
+Dot11Elt
+
+Dot11ProbeReq
+
+Dot11ProbeResp
+
+addr1	Destination MAC address.
+addr2	Source MAC address of sender.
+addr3	MAC address of Access Point.
+
+
+How to get SSID list?
+Detect beacon frames, access the 802.11 frame -> SSID parameter 
+'''
+
+
 def getClients(pkt):
-    # print(a)
+    '''Identifies beacon packets and extracts the name and BSSID address
+    @param pkt: captured packet
+
+    Based on:
+    https://www.youtube.com/watch?v=owsr3X453Z4&ab_channel=PentesterAcademyTV
+    '''
     global voc
     voc = {}  # vocabulary for all the pkt info
-    #voc[str(a)] = str(a)
     bssid = pkt[Dot11].addr3
     target_bssid = a
     if target_bssid == bssid and not pkt.haslayer(Dot11Beacon) and not pkt.haslayer(Dot11ProbeReq) and not pkt.haslayer(Dot11ProbeResp):
         if str(pkt.summary()) not in voc:
             print(pkt.summary())
         voc[str(pkt.summary())] = True
-        
 
-def sniffClients(wlan, BSSID):
+
+def sniffClients(interface, BSSID):
+    '''Detects all available users of a given AP.
+    @param interface - the given monitor card we are using for sniffing
+    @param BSSID - given Acess Point ID.
+    '''
     global a
     a = BSSID
     interupted = False
     try:
-        sniff(iface=wlan, prn=getClients, stop_filter=interupted)
+        sniff(iface=interface, prn=getClients, stop_filter=interupted)
         while True:
             time.sleep(1)
     except KeyboardInterrupt:
         interupted = True
-        
+
 
 def scanNetworks(interface):
+    '''Gets the names of all availale Wi-Fi networks around me.
+    Performs sniffing for packets on a specific monitor card.
+    Picks out only packets that has layer of Dot11 - Meaning Wi-Fi layers - specifies a 802.11 packet.
+    For each packet, extracts the AP information SSID, BBSID, channel.
+    @param interface - name of the monitor card
+    '''
     print("Press Ctrl-C to finish scanning for networks")
     # we arrange all of the networks we have found in known so that we will not print the network information twice on the terminal.
-    known = {}
+    avilable_networks = {}
 
-    def callback(frame):
-        if frame.haslayer(Dot11):
-            if frame.haslayer(Dot11Beacon) or frame.haslayer(Dot11ProbeResp):
-
-                source = frame[Dot11].addr2
-                if source not in known:  # if the network we found is not in 'known'
-                    ssid = frame[Dot11Elt][0].info  # save the ssid
+    def callback(pkt):
+        if pkt.haslayer(Dot11):  # is it a Wi-Fi packet? 802.11
+            # Beacon frame is one of the management frames in IEEE 802.11 based WLANs. It contains all the information about the network.
+            # The WLAN clients or stations use probe request frame to scan the area for availability of WLAN network.
+            if pkt.haslayer(Dot11Beacon) or pkt.haslayer(Dot11ProbeResp):
+                src = pkt[Dot11].addr2  # src mac of sender
+                if src not in avilable_networks:  # if the network we found is not in 'known'
+                    ssid = pkt[Dot11Elt][0].info  # save the ssid
                     # save the channel of the network
-                    channel = frame[Dot11Elt][2].info
+                    channel = pkt[Dot11Elt][2].info
                     # transfer it to hex numbers.
                     print(channel)
                     channel = int(codecs.getencoder('hex')(channel)[0], 16)
                     # print the network information.
                     print("SSID: '{}', BSSID: {}, channel: {}".format(
-                        ssid, source, channel))
-                    # add the network to 'known'
-                    known[source] = (ssid, channel)
+                        ssid, src, channel))
+                    # add the network to list
+                    avilable_networks[src] = (ssid, channel)
 
     sniff(iface=interface, prn=callback, store=0)
 
-    return known
+    return avilable_networks
